e9b48c4f3a7974ff5f22212eab1515e5a8bb6579
ashraf <aabusharelh@gmail.com>
2023-10-27 01:51:53 -0600

    fixed navbar

--- src/main/java/eu/openanalytics/containerproxy/api/BaseController.java
+++ src/main/java/eu/openanalytics/containerproxy/api/BaseController.java
@@ -33,6 +33,7 @@ public class BaseController {
 	protected void prepareMap(ModelMap map) {
 		map.put("title", environment.getProperty("proxy.title", "ShinyProxy"));
 		map.put("bootstrapCss", "/webjars/bootstrap/5.3.2/css/bootstrap.min.css");
+                map.put("bootstrapIconsCss", "/webjars/bootstrap-icons/1.11.1/font/bootstrap-icons.min.css");
 		map.put("bootstrapJs", "/webjars/bootstrap/5.3.2/js/bootstrap.min.js");
 		map.put("jqueryJs", "/webjars/jquery/3.7.1/jquery.min.js");
 		map.put("fontAwesomeCss", "/webjars/fontawesome/4.7.0/css/font-awesome.min.css");
--- src/main/java/eu/openanalytics/shinyproxy/controllers/BaseController.java
+++ src/main/java/eu/openanalytics/shinyproxy/controllers/BaseController.java
@@ -116,8 +116,10 @@ public abstract class BaseController {
 		}
 
 		map.put("bootstrapCss", "/webjars/bootstrap/5.3.2/css/bootstrap.min.css");
+                map.put("bootstrapIconsCss", "/webjars/bootstrap-icons/1.11.1/font/bootstrap-icons.min.css");
 		map.put("bootstrapJs", "/webjars/bootstrap/5.3.2/js/bootstrap.min.js");
 		map.put("jqueryJs", "/webjars/jquery/3.7.1/jquery.min.js");
+                map.put("fontAwesomeCss", "/webjars/fontawesome/4.7.0/css/font-awesome.min.css");
 		map.put("handlebars", "/webjars/handlebars/4.7.7/handlebars.runtime.min.js");
 
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
