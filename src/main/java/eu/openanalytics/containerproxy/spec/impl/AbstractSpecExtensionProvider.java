c4bd0b6f26c6a17ac167fd3c43952eb96dbe35bd
Ashraf Abusharekh <ashraf_abusharekh@swissre.com>
2024-10-16 19:03:29 +0000

    upgrading springboot to a newer version to address vulnerabilities

--- src/main/java/eu/openanalytics/containerproxy/spec/impl/AbstractSpecExtensionProvider.java
+++ src/main/java/eu/openanalytics/containerproxy/spec/impl/AbstractSpecExtensionProvider.java
@@ -18,17 +18,75 @@
  * You should have received a copy of the Apache License
  * along with this program.  If not, see <http://www.apache.org/licenses/>
  */
+// package eu.openanalytics.containerproxy.spec.impl;
+
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import eu.openanalytics.containerproxy.model.spec.ISpecExtension;
+// import eu.openanalytics.containerproxy.model.spec.ProxySpec;
+// import eu.openanalytics.containerproxy.spec.IProxySpecProvider;
+
+// import javax.annotation.PostConstruct;
+
+// import java.util.List;
+// import org.springframework.beans.factory.annotation.Autowired;
+
+// public class AbstractSpecExtensionProvider<T extends ISpecExtension> {
+
+//     private static final Logger logger = LoggerFactory.getLogger(AbstractSpecExtensionProvider.class);
+
+//     private List<T> specs;
+
+//     @Autowired
+//     private IProxySpecProvider proxySpecProvider;
+
+//      @PostConstruct
+//     public void postInit() {
+//         if (specs != null) {
+//             specs.forEach(specExtension -> {
+//                 ProxySpec spec = proxySpecProvider.getSpec(specExtension.getId());
+//                 if (spec != null) {
+//                     spec.addSpecExtension(specExtension);
+//                     logger.info("Successfully added specExtension to ProxySpec: id={}, displayName={}, description={}, logoURL={}",
+//                             safeGet(spec.getId()), safeGet(spec.getDisplayName()), safeGet(spec.getDescription()), safeGet(spec.getLogoURL()));
+//                 } else {
+//                     logger.error("Spec is null for specExtension with ID: {}. SpecExtension details: id={}, displayName={}, description={}, logoURL={}",
+//                             safeGet(specExtension.getId()), safeGet(specExtension.getDisplayName()), safeGet(specExtension.getDescription()), safeGet(specExtension.getLogoURL()));
+//                 }
+//             });
+//         }
+//     }
+
+//     private String safeGet(String value) {
+//         return value != null ? value : "null";
+//     }
+
+//     public void setSpecs(List<T> specs) {
+//         this.specs = specs;
+//     }
+
+//     public List<T> getSpecs() {
+//         return specs;
+//     }
+
+// }
+
+
 package eu.openanalytics.containerproxy.spec.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import eu.openanalytics.containerproxy.model.spec.ISpecExtension;
+import eu.openanalytics.containerproxy.model.spec.ProxySpec;
 import eu.openanalytics.containerproxy.spec.IProxySpecProvider;
 
 import javax.annotation.PostConstruct;
-
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class AbstractSpecExtensionProvider<T extends ISpecExtension> {
+
+    private static final Logger logger = LoggerFactory.getLogger(AbstractSpecExtensionProvider.class);
 
     private List<T> specs;
 
@@ -39,9 +97,20 @@ public class AbstractSpecExtensionProvider<T extends ISpecExtension> {
     public void postInit() {
         if (specs != null) {
             specs.forEach(specExtension -> {
-                proxySpecProvider.getSpec(specExtension.getId()).addSpecExtension(specExtension);
+                ProxySpec spec = proxySpecProvider.getSpec(specExtension.getId());
+                if (spec != null) {
+                    spec.addSpecExtension(specExtension);
+                    logger.info("Successfully added specExtension to ProxySpec: id={}, displayName={}, description={}, logoURL={}",
+                            safeGet(spec.getId()), safeGet(spec.getDisplayName()), safeGet(spec.getDescription()), safeGet(spec.getLogoURL()));
+                } else {
+                    logger.error("Spec is null for specExtension with ID: {}", safeGet(specExtension.getId()));
+                }
             });
         }
+    }
+
+    private String safeGet(String value) {
+        return value != null ? value : "null";
     }
 
     public void setSpecs(List<T> specs) {
@@ -51,5 +120,4 @@ public class AbstractSpecExtensionProvider<T extends ISpecExtension> {
     public List<T> getSpecs() {
         return specs;
     }
-
 }
